114c68196a9ab48b715a7802c4b0f8f33eced6f0
DeVictor2002 <victorhenriquebl11@gmail.com>
2024-05-05 02:11:11 -0300

    Site previsão do tempo , fiz para entender mais sobre API's.

---
+++ previsao-do-tempo/src/javascript/scripts.js
@@ -0,0 +1,23 @@
+
+const key = "62d50ff2cb7ca5fd25d288e993fad686";
+
+function viewData(data) {
+    document.querySelector('.tittle').innerHTML = data.name + ", " + data.sys.country;
+    document.querySelector('.temp').innerHTML = Math.floor(data.main.temp) + 'ºC';
+    document.querySelector('.forecast-text').innerHTML = data.weather[0].description;
+    document.querySelector('.humidity').innerHTML = 'Humidade: ' + data.main.humidity + '%';
+    document.querySelector('.forecast-img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;  
+
+    console.log(data);
+}
+
+async function searchCity(city) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json());
+    viewData(data);
+}
+
+function clickOnButton () {
+    const city = document.querySelector('.city-input').value;
+    searchCity(city);
+}
+
