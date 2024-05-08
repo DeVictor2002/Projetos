f48274ba07cdc46234a4e5b23be273500d8e939a
DeVictor2002 <victorhenriquebl11@gmail.com>
2024-05-08 04:45:30 -0300

    Formulário que fiz para aprender a manipular melhor o input do date

---
+++ formulario-fiap/src/javascript/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const birthDateInput = document.querySelector('.birthDate');
+    
+    birthDateInput.addEventListener('input', function() {
+        let value = this.value;
+        let formattedValue = '';
+        if (value.length <= 2) {
+            formattedValue = value.replace(/\D/g, '').slice(0, 2);
+        } else if (value.length <= 4) {
+            formattedValue = value.replace(/\D/g, '').slice(0, 2) + '/' + value.replace(/\D/g, '').slice(2, 4);
+        } else {
+            formattedValue = value.replace(/\D/g, '').slice(0, 2) + '/' + value.replace(/\D/g, '').slice(2, 4) + '/' + value.replace(/\D/g, '').slice(4, 8);
+        }
+        this.value = formattedValue;
+    })})
